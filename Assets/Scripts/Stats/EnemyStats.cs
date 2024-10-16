327f4cbf2bb3650bb842550628c39b60d5ee9c5d
kiyomin0615 <moeday1010@gmail.com>
2024-10-16 16:42:21 +0900

    increase enemy stats by level

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -6,8 +6,17 @@ public class EnemyStats : Stats
 {
     Enemy enemyComponent;
 
+    [Header("Level Details")]
+    [SerializeField] int level = 1;
+
+    [Range(0f, 1f)]
+    [SerializeField] float multiplier = 0.4f;
+
+
     protected override void Start()
     {
+        ApplyAllModifiers();
+
         base.Start();
 
         enemyComponent = GetComponent<Enemy>();
@@ -23,5 +32,26 @@ public class EnemyStats : Stats
         base.Die();
 
         enemyComponent.Die();
+    }
+
+    void ModifyStatByLevel(Stat stat)
+    {
+        for (int i = 1; i < level; i++)
+        {
+            stat.AddModifier((int)(stat.GetValue() * multiplier));
+        }
+    }
+
+    void ApplyAllModifiers()
+    {
+        ModifyStatByLevel(strength);
+        ModifyStatByLevel(agility);
+        ModifyStatByLevel(vitality);
+        ModifyStatByLevel(damage);
+        ModifyStatByLevel(criticalChange);
+        ModifyStatByLevel(criticalDamagePercentage);
+        ModifyStatByLevel(basicHP);
+        ModifyStatByLevel(armor);
+        ModifyStatByLevel(evasion);
     }
 }
