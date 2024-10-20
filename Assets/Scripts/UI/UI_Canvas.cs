4f71b27c98b4171474469f5f05940c6beb4d3a36
kiyomin0615 <moeday1010@gmail.com>
2024-10-20 17:19:00 +0900

    toggle or switch ui panels

---
+++ Assets/Scripts/UI/UI_Canvas.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Canvas : MonoBehaviour
+{
+    [SerializeField] GameObject characterPanelUI;
+    [SerializeField] GameObject skillPanelUI;
+    [SerializeField] GameObject craftPanelUI;
+    [SerializeField] GameObject optionsPanelUI;
+
+    void Start()
+    {
+        ActivateUIPanel(null);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SwitchToTargetUIPanel(characterPanelUI);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SwitchToTargetUIPanel(skillPanelUI);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SwitchToTargetUIPanel(craftPanelUI);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SwitchToTargetUIPanel(optionsPanelUI);
+        }
+    }
+
+    public void ActivateUIPanel(GameObject panel)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        if (panel != null)
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    public void SwitchToTargetUIPanel(GameObject targetPanel)
+    {
+        if (targetPanel != null && targetPanel.activeSelf)
+        {
+            targetPanel.SetActive(false);
+            return;
+        }
+
+        ActivateUIPanel(targetPanel);
+    }
+}
