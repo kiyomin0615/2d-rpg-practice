98cd801fc8dcd38e6ad3565bd25a6433589c932a
kiyomin0615 <moeday1010@gmail.com>
2024-10-22 21:16:37 +0900

    add save system

---
+++ Assets/Scripts/Save/DataHandler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class DataHandler
+{
+    string path = "";
+    string fileName = "";
+
+    public DataHandler(string path, string fileName)
+    {
+        this.path = path;
+        this.fileName = fileName;
+    }
+
+    public void Save(GameData gameData)
+    {
+        string fullPath = Path.Combine(this.path, this.fileName);
+
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+            string jsonData = JsonUtility.ToJson(gameData, true);
+            using (FileStream fs = new FileStream(fullPath, FileMode.Create)) // Write
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.Write(jsonData);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Failed to save data: {e}");
+        }
+    }
+
+    public GameData Load()
+    {
+        string fullPath = Path.Combine(this.path, this.fileName);
+        GameData gameData = null;
+
+        if (File.Exists(fullPath))
+        {
+            try
+            {
+                string jsonData = "";
+                using (FileStream fs = new FileStream(fullPath, FileMode.Open)) // Read
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        jsonData = sr.ReadToEnd();
+                    }
+                }
+
+                gameData = JsonUtility.FromJson<GameData>(jsonData);
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"Failed to load data: {e}");
+            }
+        }
+
+        return gameData;
+    }
+}
---
+++ Assets/Scripts/Save/SaveManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance;
+
+    GameData gameData;
+    List<ISaveManager> saveManagers = new List<ISaveManager>();
+    DataHandler dataHandler;
+
+    [SerializeField] string fileName;
+
+    void Awake()
+    {
+        if (instance != null)
+            Destroy(instance.gameObject);
+        else
+            instance = this;
+    }
+
+    void Start()
+    {
+        saveManagers = FindAllSaveManagers();
+        dataHandler = new DataHandler(Application.persistentDataPath, fileName);
+
+        LoadGame();
+    }
+
+    public void StartNewGame()
+    {
+        gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        dataHandler.Load();
+
+        foreach (ISaveManager saveManager in saveManagers)
+        {
+            saveManager.LoadData(this.gameData);
+        }
+
+        if (this.gameData == null)
+        {
+            Debug.Log("No Game Data");
+            StartNewGame();
+        }
+    }
+
+    public void SaveGame()
+    {
+        foreach (ISaveManager saveManager in saveManagers)
+        {
+            saveManager.SaveData(ref this.gameData);
+        }
+
+        dataHandler.Save(gameData);
+
+        Debug.Log("Game Saved.");
+    }
+
+    void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
+    List<ISaveManager> FindAllSaveManagers()
+    {
+        IEnumerable<ISaveManager> saveManagers = FindObjectsOfType<MonoBehaviour>().OfType<ISaveManager>(); // ?
+        return new List<ISaveManager>(saveManagers);
+    }
+}
